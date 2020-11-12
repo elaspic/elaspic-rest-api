a62a705fae89e7dfd56f55299c966acea1b82af6
Alexey Strokach <alex.strokach@utoronto.ca>
2020-11-12 14:37:50 -0500

    Fix query_mutation_data

--- src/elaspic_rest_api/jobsubmitter/elaspic2.py
+++ src/elaspic_rest_api/jobsubmitter/elaspic2.py
@@ -1,21 +1,51 @@
 from enum import Enum
-from typing import Dict, List, Tuple, Type
+from typing import Dict, List, NamedTuple, Tuple, Type
 
 import aiohttp
 from kmbio import PDB
 
 from elaspic_rest_api import jobsubmitter as js
 
-mutation_sql = """\
+
+core_mutation_local_sql = """\
 select model_filename_wt, chain_modeller, mutation_modeller
-from elaspic_webserver.elaspic_core_mutation_local
+from elaspic_core_mutation_local
 where protein_id = %(protein_id)s and mutation = %(mutation)s;
+"""
+
+interface_mutation_local_sql = """\
+select model_filename_wt, chain_modeller, mutation_modeller
+from elaspic_interface_mutation_local
+where protein_id = %(protein_id)s and mutation = %(mutation)s;
+"""
+
+core_mutation_database_sql = """\
+SELECT CONCAT(m.path_to_data, mut.model_filename_wt) as model_filename_wt,
+mut.chain_modeller, mut.mutation_modeller
+FROM elaspic_core_mutation mut
+JOIN elaspic_core_model m USING (protein_id, domain_id)
+WHERE protein_id = %(protein_id)s AND mutation = %(mutation)s;
+"""
+
+interface_mutation_database_sql = """\
+SELECT CONCAT(m.path_to_data, mut.model_filename_wt) as model_filename_wt,
+mut.chain_modeller, mut.mutation_modeller
+FROM elaspic_interface_mutation mut
+JOIN elaspic_interface_model m USING (interface_id)
+WHERE protein_id = %(protein_id)s AND mutation = %(mutation)s;
 """
 
 
 class COI(Enum):
     CORE = "core"
     INTERFACE = "interface"
+
+
+class MutationInfo(NamedTuple):
+    structure_file: str
+    chain: str
+    mutation: str
+    coi: COI
 
 
 async def elaspic2(ds: js.DataStructures) -> None:
@@ -29,37 +59,33 @@ async def elaspic2(ds: js.DataStructures) -> None:
             js.remove_from_monitored(item, ds.monitored_jobs)
 
 
-async def query_mutation_data(item: js.Item) -> List[Tuple[str, str, str, COI]]:
+async def query_mutation_data(item: js.Item) -> List[MutationInfo]:
     args = item.args
-    DBConnection: Type[js.db._DBConnection]
-
-    if args["job_type"] == "database":
-        DBConnection = js.EDBConnection
-        core_mutation_table = "elaspic.elaspic_domain_mutation"
-        interface_mutation_table = "elaspic.elaspic_domain_pair_mutation"
-    else:
-        assert args["job_type"] == "local"
-        DBConnection = js.WDBConnection
-        core_mutation_table = "elaspic_webserver.elaspic_core_mutation_local"
-        interface_mutation_table = "elaspic_webserver.elaspic_interface_mutation_local"
-
     kwargs = {"protein_id": args["protein_id"], "mutation": args["mutations"]}
     assert "," not in kwargs["mutation"]
 
-    async with DBConnection() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute(mutation_sql.format(mutation_table=core_mutation_table), kwargs)
-            core_mutation_values = await cur.fetchall()
+    if args["job_type"] == "database":
+        core_mutation_sql = core_mutation_database_sql
+        interface_mutation_sql = core_mutation_database_sql
+    else:
+        assert args["job_type"] == "local"
+        core_mutation_sql = core_mutation_local_sql
+        interface_mutation_sql = interface_mutation_local_sql
 
-            await cur.execute(mutation_sql.format(mutation_table=interface_mutation_table), kwargs)
-            interface_mutation_values = await cur.fetchall()
+    async with js.WDBConnection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(core_mutation_sql, kwargs)
+            core_mutation_values: List[Tuple[str, str, str]] = await cur.fetchall()
+
+            await cur.execute(interface_mutation_sql, kwargs)
+            interface_mutation_values: List[Tuple[str, str, str]] = await cur.fetchall()
 
     mutation_values = (
         #
-        [tuple(values) + (COI.CORE,) for values in core_mutation_values]
-        + [tuple(values) + (COI.INTERFACE,) for values in interface_mutation_values]
+        [MutationInfo(*values, COI.CORE) for values in core_mutation_values]
+        + [MutationInfo(*values, COI.INTERFACE) for values in interface_mutation_values]
     )
-    return mutation_values  # type: ignore
+    return mutation_values
 
 
 def extract_protein_info(structure_file: str, chain: str, mutation: str, coi: COI) -> Dict:
--- tests/jobsubmitter/test_elaspic2.py
+++ tests/jobsubmitter/test_elaspic2.py
@@ -1,11 +1,39 @@
-from elaspic_rest_api import jobsubmitter as js
+from pathlib import Path
+
 import pytest
 
+from elaspic_rest_api import config
+from elaspic_rest_api import jobsubmitter as js
+from elaspic_rest_api.jobsubmitter.elaspic2 import COI
+
+
 @pytest.mark.asyncio
-async def test_query_mutation_data():
+async def test_query_mutation_data_local():
     item = js.Item(
         run_type="mutations",
         args={"job_id": 1, "job_type": "local", "protein_id": "d822d7", "mutations": "P37A"},
     )
-    values = await js.elaspic2.query_mutation_data(item)
-    print(values)
+    mutation_info_list = await js.elaspic2.query_mutation_data(item)
+    assert len(mutation_info_list) >= 2
+    for idx, mutation_info in enumerate(mutation_info_list):
+        if idx == 0:
+            assert mutation_info.coi == COI.CORE
+        else:
+            assert mutation_info.coi == COI.INTERFACE
+        assert Path(config.DATA_DIR).joinpath(mutation_info.structure_file).is_file()
+
+
+@pytest.mark.asyncio
+async def test_query_mutation_data_database():
+    item = js.Item(
+        run_type="mutations",
+        args={"job_id": 1, "job_type": "database", "protein_id": "A0AV96", "mutations": "V274M"},
+    )
+    mutation_info_list = await js.elaspic2.query_mutation_data(item)
+    assert len(mutation_info_list) >= 2
+    for idx, mutation_info in enumerate(mutation_info_list):
+        if idx == 0:
+            assert mutation_info.coi == COI.CORE
+        else:
+            assert mutation_info.coi == COI.INTERFACE
+        assert Path(config.DATA_DIR).joinpath(mutation_info.structure_file).is_file()
