eecd497ce6774cd00360dc92d1a8a882dc47a71d
ostrokach <ostrokach@gmail.com>
2021-01-05 11:13:25 -0500

    0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(file):
 
 setup(
     name="elaspic-rest-api",
-    version="0.2.3",
+    version="0.2.4",
     description="ELASPIC REST API",
     long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
