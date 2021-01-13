794b00d464186902a73c468fae5adb754ed1f152
ostrokach <ostrokach@gmail.com>
2021-01-13 14:27:57 -0500

    Fix missing import

--- src/elaspic_rest_api/jobsubmitter/__init__.py
+++ src/elaspic_rest_api/jobsubmitter/__init__.py
@@ -10,7 +10,7 @@ from .finalize import (
     set_db_errors,
 )
 from .jobsubmitter import parse_input_data, start_jobsubmitter, submit_job
-from .monitor import qstat, show_stats, validation
+from .monitor import qstat, validation
 from .precalculated import check_prereqs, persist_precalculated, update_precalculated
 from .submit import pre_qsub, qsub
 from .utils import remove_from_monitored, restart_or_drop
